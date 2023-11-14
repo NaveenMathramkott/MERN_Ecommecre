8fc33a52f0f84d1af00b4e519443c4d517feae93
naveen_mathramkott <nmathramkott@fintech.net>
2023-11-14 21:09:20 +0530

    from ios

---
+++ client/src/pages/Category.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layouts/Layout";
+
+const Category = () => {
+  return (
+    <>
+      <Layout title={`Category Ecommerce app`}>
+        <h4>Category</h4>
+      </Layout>
+    </>
+  );
+};
+
+export default Category;
